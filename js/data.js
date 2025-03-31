e939a90c4828c7faf306bceef2867348432435bf
binijjang <ru0dn7@gmail.com>
2025-03-31 17:15:51 +0900

    sub4 Page Json 적용

---
+++ js/data.js
@@ -0,0 +1,94 @@
+// data.js
+
+const videos = [
+  {
+      "title": "Udo Island",
+      "image": "./images/sub04/thumb05.png",
+      "youtube_url": "https://www.youtube.com/watch?v=CU-0JC_mawg",
+      "alt_text": "우도"
+  },
+  {
+      "title": "Andeok Valley",
+      "image": "./images/sub04/thumb06.png",
+      "youtube_url": "https://www.youtube.com/watch?v=eOtRkT4YK7I",
+      "alt_text": "안덕계곡"
+  },
+  {
+      "title": "Sanbang Mountain",
+      "image": "./images/sub04/thumb07.png",
+      "youtube_url": "https://www.youtube.com/watch?v=k8rx8GAofZA",
+      "alt_text": "산방산"
+  },
+  {
+      "title": "Camellia Garden",
+      "image": "./images/sub04/thumb08.png",
+      "youtube_url": "https://www.youtube.com/watch?v=ZyOAjuWznpY",
+      "alt_text": "동백동산"
+  },
+  {
+      "title": "Saryani Forest Road",
+      "image": "./images/sub04/thumb09.png",
+      "youtube_url": "https://www.youtube.com/watch?v=5CHO4t28GFM",
+      "alt_text": "사려니숲길"
+  },
+  {
+      "title": "Sunrise on Mt. Halla",
+      "image": "./images/img04-15.png",
+      "youtube_url": "https://www.youtube.com/watch?v=nh9ub9wA1nc",
+      "alt_text": "한라산 일출"
+  },
+  {
+      "title": "Sunset on Mt. Halla",
+      "image": "./images/sub04/thumb02.png",
+      "youtube_url": "https://www.youtube.com/watch?v=r1iJHT7NYto",
+      "alt_text": "한라산일몰"
+  },
+  {
+      "title": "Cheona Valley",
+      "image": "./images/sub04/thumb03.png",
+      "youtube_url": "https://www.youtube.com/watch?v=cEmpLvsSpjk",
+      "alt_text": "천아계곡"
+  },
+  {
+      "title": "Cheonjiyeon Nandaerim",
+      "image": "./images/sub04/thumb04.png",
+      "youtube_url": "https://www.youtube.com/watch?v=KrOG55uTtz0",
+      "alt_text": "천지연 난대림"
+  },
+  {
+      "title": "Yongcheon Cave",
+      "image": "./images/img04-11.png",
+      "youtube_url": "https://www.youtube.com/watch?v=nh9ub9wA1nc",
+      "alt_text": "용천동굴"
+  },
+  {
+      "title": "Chunjeyeon Waterfall",
+      "image": "./images/img04-16.png",
+      "youtube_url": "https://www.youtube.com/watch?v=9ipu4afUOTw",
+      "alt_text": "천지연 폭포"
+  },
+  {
+      "title": "Marado Island",
+      "image": "./images/img04-13.png",
+      "youtube_url": "https://www.youtube.com/watch?v=LqxDUN4WdPw",
+      "alt_text": "마라도"
+  },
+  {
+      "title": "Yongnuni Oreum",
+      "image": "./images/img04-14.png",
+      "youtube_url": "https://www.youtube.com/watch?v=ycVCQdHBK5Y",
+      "alt_text": "용누니 오름"
+  },
+  {
+      "title": "Muljangori Oreum",
+      "image": "./images/img04-12.png",
+      "youtube_url": "https://www.youtube.com/watch?v=gL3BiFwNXB0",
+      "alt_text": "물정리 오름"
+  },
+  {
+      "title": "Chagwido Island",
+      "image": "./images/sub04/thumb01.png",
+      "youtube_url": "https://www.youtube.com/watch?v=2gKIQWlAhyM",
+      "alt_text": "차귀도"
+  }
+];
